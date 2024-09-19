c360920928ff14f685c595688d46884c875d585e
Kushi653 <kushiyes653@gmail.com>
2024-09-18 22:55:32 -0500

    Initial commit

---
+++ src/main/java/com/Employee/EmployeeSystem/repository/EmpRepo.java
@@ -0,0 +1,11 @@
+package com.Employee.EmployeeSystem.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.Employee.EmployeeSystem.entity.Employee;
+@Repository
+
+public interface EmpRepo extends JpaRepository<Employee,Integer>{
+
+}
